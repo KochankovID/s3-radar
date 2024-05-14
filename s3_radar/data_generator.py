5a7855f5638e9b0e9d978ad7361f5e6d74891345
Илья Кочанков <ilyakochankov@yandex.ru>
2024-05-14 15:51:02 +0300

    :bug: fix persissions

--- s3_radar/data_generator.py
+++ s3_radar/data_generator.py
@@ -1,4 +1,5 @@
 import tempfile
+import uuid
 from pathlib import Path
 
 
@@ -7,8 +8,12 @@ def build_target_file_path(local_file_path: Path) -> str:
 
 
 def generate_file(byte_size: int) -> Path:
-    with tempfile.NamedTemporaryFile(delete=False) as file:
-        file.write(generate_string(byte_size).encode())
+    try:
+        with tempfile.NamedTemporaryFile(delete=False) as file:
+            file.write(generate_string(byte_size).encode())
+    except BaseException:
+        with Path(f"tmp-gen-file-{uuid.uuid4()!s}").open(mode="w+b") as file:
+            file.write(generate_string(byte_size).encode())
     return Path(file.name)
 
 
